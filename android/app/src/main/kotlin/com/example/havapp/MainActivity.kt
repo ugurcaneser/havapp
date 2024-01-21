036e19e10878f1d9ae6b46ec8af1edbd52579984
ugurcaneser <ugurcaneser@icloud.com>
2024-01-21 23:38:54 +0300

    created project

---
+++ android/app/src/main/kotlin/com/example/havapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.havapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: const Text("Havapp"),
+      ),
+      body: const Center(
+        child: Text("Work in progress..."),
+      ),
+    );
+  }
+}
